ebffe9db359fca3f3fcc4302b06446623c28efa6
mrwerner392 <mrwerner392@gmail.com>
2019-08-11 18:57:02 -0400

    functioning properly  no bonus

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,9 +4,6 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
-  response_hash = JSON.parse(response_string)
-
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,15 +13,56 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
+  
+  url = 'http://www.swapi.co/api/people/'
+  character_hash = nil
+  
+  loop do
+
+    response_string = RestClient.get(url).body
+    response_hash = JSON.parse(response_string)
+    character_hash = response_hash["results"].find do |character|
+      character["name"].downcase == character_name
+    end
+    
+  
+    if character_hash
+      break
+    elsif !response_hash["next"]
+      break
+    else
+      url = response_hash["next"]
+    end
+  end
+
+  if character_hash
+    character_hash["films"].map do |film|
+      response_string = RestClient.get(film).body
+      response_hash = JSON.parse(response_string)
+      response_hash
+    end
+  else
+    return nil
+  end
+
 end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each_with_index do |film, index|
+    puts "#{index + 1}. #{film["title"]}"
+  end
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
-  print_movies(films)
+  if films
+    print_movies(films)
+  else
+    puts "Invalid character."
+    character = get_character_from_user
+    show_character_movies(character)
+  end
 end
 
 ## BONUS
